4604ddc98c46c076bffb8a9eb0459ee471f0c3f8
Damon Allen <damon.nicholas.allen@live.com>
2023-02-01 18:14:25 -0500

    fourth event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './less/index.less'
 
 // Your code goes here!
+
+let header = document.querySelector('h1');
+let page = document;
+
+//First Event Listener
+
+page.addEventListener('wheel', (event) =>{
+    let number = Math.floor(Math.random() * 10);
+    console.log(number);
+    if(number >= 5) {
+        header.style.color = 'blue';
+    } else if(number < 5) {
+        header.style.color = 'yellow';
+    }
+});
+
+//Second Event Listener
+
+let navigation = document.querySelectorAll('a');
+
+navigation.forEach((a) => {
+    a.addEventListener('mouseenter', (event) =>{
+        a.style.fontSize = "24px";
+    })
+});
+
+//Third Event Listener
+
+navigation.forEach((a) => {
+    a.addEventListener('mouseleave', (event) =>{
+        a.style.fontSize = "16px";
+    })
+});
+
+//Fourth Event Listener
+
+let CTA = document.querySelectorAll('.btn');
+console.log(CTA);
+
+CTA.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        alert('you have been signed up!');
+    })
+});
+
+
